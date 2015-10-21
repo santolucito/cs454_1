9e51c5694284ac143b3017d72eae637d1c8ddfe8
Mark Santolucito <mark.santolucito@yale.edu>
2015-10-21 17:01:32 -0400

    translate to LFGC language

--- cs454/vcgen/src/vcgen.scala
+++ cs454/vcgen/src/vcgen.scala
@@ -4,6 +4,7 @@ import java.io.FileReader
 
 object VCGen {
 
+  var cnt = 0
   /* Arithmetic expressions. */
   trait ArithExp
 
@@ -46,7 +47,6 @@ object VCGen {
 
   /* Complete programs. */
   type Program = Product4[String, List[PreCondition], List[PostCondition], Block]
-
 
   object ImpParser extends RegexParsers {
     /* General helpers. */
@@ -122,9 +122,105 @@ object VCGen {
       }
   }
 
+  /*************** Gaurded Command Language ****************/
+
+  /* Comamnds */
+  trait Command
+
+  case class Assume(b: BoolExp) extends Command
+  case class Assert(b: BoolExp) extends Command
+  case class Havoc(x: Var) extends Command
+  case class Compose(c1: Command, c2: Command) extends Command
+  case class Or(c1: Command, c2: Command) extends Command
+
+  def translate(p: Program): Command = {
+    return translateBlock(p._4)
+  }
+
+  def translateBlock(b: Block): Command = {
+    //GC(c1; c2) =GC(c1) ; GC(c2)
+    //allVars(b).contains(Var("fresh_"i+*)
+    if(b.isEmpty){
+      return Assume(BCmp(Num(42),"=",Num(42)))
+    }
+    else {
+      val listCmds = b.map (x => translateStatement(x))
+      return composeL(listCmds)
+    }
+  }
+
+  def composeL(cs: List[Command]) : Command = {
+    return (cs.tail).foldLeft (cs.head) (Compose)
+  }
+
+  def replace(f: Var, old: Var, e:ArithExp): ArithExp = {
+    e match {
+      case Num(v) => Num(v)
+      case Var(name) => if (name == old){ f } else Var(name)
+      case Add(l,r) => Add(replace(f,old,l),replace(f,old,r))
+      case Sub(l,r) => Sub(replace(f,old,l),replace(f,old,r))
+      case Mul(l,r) => Mul(replace(f,old,l),replace(f,old,r))
+      case Div(l,r) => Div(replace(f,old,l),replace(f,old,r))
+      case Mod(l,r) => Mod(replace(f,old,l),replace(f,old,r))
+      case Parens(a) => Parens(replace(f,old,a))
+    }
+  }
+
+  def allVars_Statement(s: Statement) : List[Var] = {
+    s match {
+      case Assign(x,e) =>
+        return List(Var(x))
+      case If(c,th,el) =>
+        return allVars(th) ++ allVars(el)
+      case While(c,i,b) =>
+        return allVars(b)
+    }
+  }
+
+  def allVars(b: Block) : List[Var] = {
+    return (b.map (allVars_Statement)).flatten.distinct
+  }
+
+  def translateStatement(s : Statement) : Command = {
+    s match {
+      //GC(x:=e) = assume tmp = x; havoc x; assume (x = e[tmp/x]) where temp is fresh
+      case Assign(x,e) =>
+        var freshVar = Var("fresh_"+cnt)
+        cnt=cnt+1
+        var xVar = Var(x)
+        var c1 = Assume(BCmp((freshVar,"=",xVar)))
+        var c2 = Havoc(xVar)
+        var c3 = Assume(BCmp((xVar,"=",replace(freshVar,xVar,e))))
+        return composeL(List(c1,c2,c3))
+
+      //GC(if b then c1 else c2) =(assume b; GC(c1)) [] (assume :b; GC(c2))
+      case If(c,th,el) =>
+        var l = Compose(Assume (c),translateBlock(th))
+        var r = Compose(Assume (BNot (c)),translateBlock(el))
+        return Or(l, r)
+
+      //GC({I} while b do c) = ?
+      case While(c,i,b) =>
+        var c1 =
+          if (i.isEmpty) Assert(BCmp((Num(43),"=",Num(43))))
+          else composeL (i.map (x => Assert(x)))
+        //var c2 = allVars b
+        var c3 = composeL ((allVars(b).map (x => Havoc(x))))
+        if (i.isEmpty) {c1 = Assume(BCmp((Num(43),"=",Num(43))))}
+        else c3 = composeL (i.map (x => Assume(x)))
+        var c4p1 = composeL (List(Assume(c),translateBlock(b),c1,Assume(BCmp(Num(44),"=",Num(45)))))
+        var c4p2 = Assume(BNot(c))
+        var c4 = Or(c4p1,c4p2)
+        return composeL (List(c1,c3,c4))
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val reader = new FileReader(args(0))
     import ImpParser._;
-    println(parseAll(prog, reader))
+    var progAST = parseAll(prog, reader)
+    var gaurdedCmd = translate(progAST.get) 
+    println(gaurdedCmd)
+
   }
 }
